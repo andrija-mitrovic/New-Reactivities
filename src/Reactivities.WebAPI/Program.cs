29c3ccc785e67503689c83c788c508f9c86b5259
Andrija Mitrovic <andrija.mitrovic@bild-studio.net>
2021-09-26 08:37:30 +0200

    Seeding data added

--- src/Reactivities.WebAPI/Program.cs
+++ src/Reactivities.WebAPI/Program.cs
@@ -1,19 +1,42 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Reactivities.Infrastructure.Data;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Reactivities.WebAPI
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using var scope = host.Services.CreateScope();
+
+            var services = scope.ServiceProvider;
+
+            var logger = services.GetRequiredService<ILogger<Program>>();
+
+            try
+            {
+                logger.LogInformation("Program.Main - Seeding data started.");
+
+                var context = services.GetRequiredService<ApplicationDbContext>();
+                await context.Database.MigrateAsync();
+                await ApplicationDbContextSeed.SeedDataAsync(context);
+
+                logger.LogInformation("Program.Main - Seeding data finished.");
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Program.Main - An error occured during migration.");
+            }
+
+            await host.RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
