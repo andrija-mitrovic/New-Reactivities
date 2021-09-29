a707ede12f92831dae8d645409a1c5753d975c23
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2021-09-29 08:48:52 +0200

    Create activitycommandhandler added

---
+++ src/Reactivities.Application/Features/Activities/Commands/CreateActivityCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Reactivities.Domain.Entities;
+
+namespace Reactivities.Application.Features.Activities.Commands
+{
+    public class CreateActivityCommand : IRequest
+    {
+        public Activity Activity { get; set; }
+    }
+}
---
+++ src/Reactivities.Application/Features/Activities/Handlers/CreateActivityHandler.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using Reactivities.Application.Features.Activities.Commands;
+using Reactivities.Infrastructure.Data;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Reactivities.Application.Features.Activities.Handlers
+{
+    public class CreateActivityHandler : IRequestHandler<CreateActivityCommand>
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<CreateActivityHandler> _logger;
+
+        public CreateActivityHandler(ApplicationDbContext context,
+            ILogger<CreateActivityHandler> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        public async Task<Unit> Handle(CreateActivityCommand request, CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("CreateActivityHandler.Handler - Creating activity.");
+
+            _context.Activities.Add(request.Activity);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
