3edcbaf9a6fab168da86c846ff244c2e8220e52e
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2021-09-30 08:29:16 +0200

    DeleteActivityHandler added

---
+++ src/Reactivities.Application/Features/Activities/Commands/DeleteActivityCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using System;
+
+namespace Reactivities.Application.Features.Activities.Commands
+{
+    public class DeleteActivityCommand : IRequest
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ src/Reactivities.Application/Features/Activities/Handlers/DeleteActivityHandler.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Reactivities.Application.Features.Activities.Commands;
+using Reactivities.Infrastructure.Data;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Reactivities.Application.Features.Activities.Handlers
+{
+    public class DeleteActivityHandler : IRequestHandler<DeleteActivityCommand>
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<DeleteActivityCommand> _logger;
+
+        public DeleteActivityHandler(ApplicationDbContext context,
+            ILogger<DeleteActivityCommand> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        public async Task<Unit> Handle(DeleteActivityCommand request, CancellationToken cancellationToken)
+        {
+            _logger.LogInformation($"DeleteActivityHandler.Handle - Deleteing activity with id={request.Id}.");
+
+            var activity = await _context.Activities.FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken);
+
+            _context.Activities.Remove(activity);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
