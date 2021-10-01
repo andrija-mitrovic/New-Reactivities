99f729a958ff1f868296daf6ca7f4d36fc4a3cf2
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2021-10-01 08:38:44 +0200

    EditActivityHandler added

---
+++ src/Reactivities.Application/Features/Activities/Commands/EditActivityCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Reactivities.Domain.Entities;
+
+namespace Reactivities.Application.Features.Activities.Commands
+{
+    public class EditActivityCommand : IRequest
+    {
+        public Activity Activity { get; set; }
+    }
+}
---
+++ src/Reactivities.Application/Features/Activities/Handlers/EditActivityHandler.cs
@@ -0,0 +1,41 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Reactivities.Application.Features.Activities.Commands;
+using Reactivities.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Reactivities.Application.Features.Activities.Handlers
+{
+    public class EditActivityHandler : IRequestHandler<EditActivityCommand>
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<EditActivityHandler> _logger;
+
+        public EditActivityHandler(ApplicationDbContext context,
+            ILogger<EditActivityHandler> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        public async Task<Unit> Handle(EditActivityCommand request, CancellationToken cancellationToken)
+        {
+            _logger.LogInformation($"EditActivityHandler.Handle - Editing activity with id={request.Activity.Id}.");
+
+            var activity = await _context.Activities
+                .FirstOrDefaultAsync(x => x.Id == request.Activity.Id, cancellationToken);
+
+            activity.Id = request.Activity.Id;
+
+            await _context.SaveChangesAsync();
+
+            return Unit.Value;
+        }
+    }
+}
