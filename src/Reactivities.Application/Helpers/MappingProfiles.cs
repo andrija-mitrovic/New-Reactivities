0995f96a436cda22bcad29fe30fb81165b20ffe6
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2021-10-03 19:52:34 +0200

    Mapping added

---
+++ src/Reactivities.Application/Helpers/MappingProfiles.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Reactivities.Domain.Entities;
+
+namespace Reactivities.Application.Helpers
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Activity, Activity>();
+        }
+    }
+}
