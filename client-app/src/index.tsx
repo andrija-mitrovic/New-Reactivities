e32bf7615388871fb89189126aee980409abcb4e
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2021-10-10 10:49:08 +0200

    Using interceptor to handle API error response

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -6,8 +6,11 @@ import "semantic-ui-css/semantic.min.css";
 import "react-toastify/dist/ReactToastify.min.css";
 import App from "./app/layout/App";
 import reportWebVitals from "./reportWebVitals";
+import { createBrowserHistory } from "history";
 import { store, StoreContext } from "./app/stores/store";
 import {BrowserRouter} from "react-router-dom";
+
+export const history = createBrowserHistory();
 
 ReactDOM.render(
   <StoreContext.Provider value={store}>
