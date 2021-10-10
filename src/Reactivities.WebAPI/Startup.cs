a0715ba79c161a973e96dbfb5efeb55e587582dd
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2021-10-10 10:28:41 +0200

    Handling exception

--- src/Reactivities.WebAPI/Startup.cs
+++ src/Reactivities.WebAPI/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Reactivities.Application.Features.Activities.Commands;
 using Reactivities.WebAPI.Extensions;
+using Reactivities.WebAPI.Middleware;
 
 namespace Reactivities.WebAPI
 {
@@ -31,9 +32,10 @@ namespace Reactivities.WebAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseMiddleware<ExceptionMiddleware>();
+
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Reactivities.WebAPI v1"));
             }
