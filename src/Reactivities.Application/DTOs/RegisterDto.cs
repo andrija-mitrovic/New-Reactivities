bc722b96a8624f103d8f67e99143831f316defc9
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2021-10-12 08:12:28 +0200

    DTOs added

---
+++ src/Reactivities.Application/DTOs/RegisterDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reactivities.Application.DTOs
+{
+    public class RegisterDto
+    {
+        public string DisplayName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string Username { get; set; }
+    }
+}
