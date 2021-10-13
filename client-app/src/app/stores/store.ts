2233858d228af994ad33ebda68740e92878b3ea3
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2021-10-13 09:05:08 +0200

    Added user store

--- client-app/src/app/stores/store.ts
+++ client-app/src/app/stores/store.ts
@@ -1,15 +1,18 @@
 import ActivityStore from "./activityStore";
 import CommonStore from "./commonStore";
+import UserStore from "./userStore";
 import { createContext, useContext } from "react";
 
 interface Store {
     activityStore: ActivityStore;
     commonStore: CommonStore;
+    userStore: UserStore;
 }
 
 export const store: Store = {
     activityStore: new ActivityStore(),
-    commonStore: new CommonStore()
+    commonStore: new CommonStore(),
+    userStore: new UserStore()
 }
 
 export const StoreContext = createContext(store);
