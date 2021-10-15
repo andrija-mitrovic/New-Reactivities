44b900c3364ee2b5518c9bcd53ac71c2c40d9505
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2021-10-15 12:59:17 +0200

    Updated CreateActivityHandler

--- src/Reactivities.Application/Features/Activities/Handlers/CreateActivityHandler.cs
+++ src/Reactivities.Application/Features/Activities/Handlers/CreateActivityHandler.cs
@@ -1,7 +1,10 @@
 ﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Reactivities.Application.Features.Activities.Commands;
 using Reactivities.Application.Helpers;
+using Reactivities.Application.Interfaces;
+using Reactivities.Domain.Entities;
 using Reactivities.Infrastructure.Data;
 using System.Threading;
 using System.Threading.Tasks;
@@ -11,12 +14,15 @@ namespace Reactivities.Application.Features.Activities.Handlers
     public class CreateActivityHandler : IRequestHandler<CreateActivityCommand, Result<Unit>>
     {
         private readonly ApplicationDbContext _context;
+        private readonly IUserAccessor _userAccessor;
         private readonly ILogger<CreateActivityHandler> _logger;
 
         public CreateActivityHandler(ApplicationDbContext context,
+            IUserAccessor userAccessor,
             ILogger<CreateActivityHandler> logger)
         {
             _context = context;
+            _userAccessor = userAccessor;
             _logger = logger;
         }
 
@@ -24,7 +30,16 @@ namespace Reactivities.Application.Features.Activities.Handlers
         {
             _logger.LogInformation("CreateActivityHandler.Handler - Creating activity.");
 
-            await _context.Activities.AddAsync(request.Activity, cancellationToken);
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.UserName == _userAccessor.GetUsername(), cancellationToken);
+
+            var attendee = new ActivityAttendee
+            {
+                AppUser = user,
+                Activity = request.Activity,
+                IsHost = true
+            };
+
+            request.Activity.ActivityAttendees.Add(attendee);
 
             var result = await _context.SaveChangesAsync(cancellationToken) > 0;
 
