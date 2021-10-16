6a74b2f15bc67998952fd02f6f313ed37527662e
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2021-10-16 08:34:02 +0200

    ProfileDto added

---
+++ src/Reactivities.Application/DTOs/ProfileDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Reactivities.Application.DTOs
+{
+    public class ProfileDto
+    {
+        public string Username { get; set; }
+        public string DisplayName { get; set; }
+        public string Bio { get; set; }
+        public string Image { get; set; }
+    }
+}
