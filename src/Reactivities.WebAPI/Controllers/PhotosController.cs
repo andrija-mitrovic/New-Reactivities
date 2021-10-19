c163f5b25a12065fce1df642b67ca60e1a62e72d
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2021-10-19 09:36:18 +0200

    PhotosController added

---
+++ src/Reactivities.WebAPI/Controllers/PhotosController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Reactivities.Application.Features.Photos.Commands;
+using System.Threading.Tasks;
+
+namespace Reactivities.WebAPI.Controllers
+{
+    public class PhotosController : BaseApiController
+    {
+        [HttpPost]
+        public async Task<IActionResult> Add([FromForm] CreatePhotoCommand command)
+        {
+            return HandleResult(await Mediator.Send(command));
+        }
+    }
+}
