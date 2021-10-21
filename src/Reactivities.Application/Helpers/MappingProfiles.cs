15aedfe22dcc1e20eccd5b389f522e2e02400175
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2021-10-21 08:19:03 +0200

    CommentDto added and configured in mapping profiles

--- src/Reactivities.Application/Helpers/MappingProfiles.cs
+++ src/Reactivities.Application/Helpers/MappingProfiles.cs
@@ -20,7 +20,11 @@ namespace Reactivities.Application.Helpers
                 .ForMember(x => x.Bio, y => y.MapFrom(z => z.AppUser.Bio))
                 .ForMember(x => x.Image, y => y.MapFrom(z => z.AppUser.Photos.FirstOrDefault(w => w.IsMain).Url));
             CreateMap<AppUser, ProfileDto>()
-                .ForMember(x=>x.Image, y=>y.MapFrom(z=>z.Photos.FirstOrDefault(w=>w.IsMain).Url));
+                .ForMember(x => x.Image, y => y.MapFrom(z => z.Photos.FirstOrDefault(w => w.IsMain).Url));
+            CreateMap<Comment, CommentDto>()
+                .ForMember(x => x.DisplayName, y => y.MapFrom(z => z.Author.DisplayName))
+                .ForMember(x => x.Username, y => y.MapFrom(z => z.Author.UserName))
+                .ForMember(x => x.Image, y => y.MapFrom(z => z.Author.Photos.FirstOrDefault(w => w.IsMain).Url));
         }
     }
 }
