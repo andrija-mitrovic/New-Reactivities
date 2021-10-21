423c45965bf536b0c2645b9ac1ef46382132bdf7
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2021-10-21 17:56:03 +0200

    Connected to the hub

--- src/Reactivities.WebAPI/Extensions/ApplicationServiceExtensions.cs
+++ src/Reactivities.WebAPI/Extensions/ApplicationServiceExtensions.cs
@@ -27,7 +27,7 @@ namespace Reactivities.WebAPI.Extensions
             {
                 opt.AddPolicy("CorsPolicy", policy =>
                 {
-                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                    policy.AllowAnyMethod().AllowAnyHeader().AllowCredentials().WithOrigins("http://localhost:3000");
                 });
             });
 
