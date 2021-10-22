bcf71645b6e6868ac05db0fe1e218cc0d14e3fca
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2021-10-22 09:11:43 +0200

    FollowController added

---
+++ src/Reactivities.WebAPI/Controllers/FollowController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Reactivities.Application.Features.Followers.Commands;
+using System.Threading.Tasks;
+
+namespace Reactivities.WebAPI.Controllers
+{
+    public class FollowController : BaseApiController
+    {
+        [HttpPost("{username}")]
+        public async Task<IActionResult> Follow(string username)
+        {
+            return HandleResult(await Mediator.Send(new FollowToggleCommand { TargetUsername = username }));
+        }
+    }
+}
